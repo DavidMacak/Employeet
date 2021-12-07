226b98733654e93a2d0f6aeb26ab94169b13e56f
David Macák <dmacak@email.cz>
2021-12-07 17:14:29 +0100

    Initial commit

---
+++ EmployeetLib/EmployeeManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeetLib
+{
+    public class EmployeeManager
+    {
+        /// <returns>String shortened to your specific number. If is string shorter, X will append.</returns>
+        public string GetEmployeeId(string firstname, string lastname)
+        {
+            string employeeId = $"{GetPartOfName(firstname, 5)}{GetPartOfName(lastname, 5)}{DateTime.Now.Millisecond.ToString()}";
+            return employeeId;
+        }
+
+        private string GetPartOfName(string name, int numberOfCharacters)
+        {
+            string output = name;
+            if (name.Length >= numberOfCharacters && name.Length != 0)
+            {
+                output = name.Substring(0, numberOfCharacters);
+            }
+            else if(name.Length == 0 && numberOfCharacters == 4)                                //Easter Egg
+            {
+                output = "Yeet";
+            }
+            else
+            {
+                do
+                {
+                    output += "X";
+                } while (output.Length < numberOfCharacters);
+            }
+
+            return output;
+        }
+
+
+    }
+}
---
+++ EmployeetWPF/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using EmployeetLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmployeetWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void generateIdBtn_Click(object sender, RoutedEventArgs e)
+        {
+            EmployeeManager employeeManager = new EmployeeManager();
+
+            generatedId.Text = employeeManager.GetEmployeeId(firstnameBox.Text, lastnameBox.Text);
+        }
+    }
+}
